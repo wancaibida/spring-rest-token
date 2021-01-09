f0ef0f24affa9c7ec811061fc13211d254821638
wancaibida <wancaibida@gmail.com>
2021-01-09 22:21:16 +0800

    Separate logic

--- src/main/kotlin/me/w2x/rest/config/ProjectConfig.kt
+++ src/main/kotlin/me/w2x/rest/config/ProjectConfig.kt
@@ -4,22 +4,17 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
 
 /**
  * Created by Charles Chen on 1/8/21.
  */
 @Configuration
-class ProjectConfig : WebSecurityConfigurerAdapter() {
+class ProjectConfig(
+    val usernamePasswordAuthenticationProvider: UsernamePasswordAuthenticationProvider
+) : WebSecurityConfigurerAdapter() {
 
     override fun configure(auth: AuthenticationManagerBuilder) {
-
-        auth.inMemoryAuthentication()
-            .withUser("admin")
-            .password("admin")
-            .authorities("ROLE_ADMIN")
-            .and()
-            .passwordEncoder(NoOpPasswordEncoder.getInstance())
+        auth.authenticationProvider(usernamePasswordAuthenticationProvider)
     }
 
     override fun configure(http: HttpSecurity) {
--- src/main/kotlin/me/w2x/rest/config/UsernamePasswordAuthenticationProvider.kt
+++ src/main/kotlin/me/w2x/rest/config/UsernamePasswordAuthenticationProvider.kt
@@ -5,19 +5,26 @@ import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 /**
  * Created by Charles Chen on 1/8/21.
  */
 @Component
-class UsernamePasswordAuthenticationProvider : AuthenticationProvider {
+class UsernamePasswordAuthenticationProvider(
+    val userDetailsService: UserDetailsService,
+    val passwordEncoder: PasswordEncoder
+) : AuthenticationProvider {
 
     override fun authenticate(authentication: Authentication): Authentication {
         val username = authentication.name
         val password = authentication.credentials.toString()
 
-        if ("admin" == username && "admin" == password) {
+        val user = userDetailsService.loadUserByUsername(username)
+
+        if (passwordEncoder.matches(password, user.password)) {
             return UsernamePasswordAuthenticationToken(
                 username,
                 password,
